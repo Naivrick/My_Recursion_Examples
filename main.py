888f070edc93d5985515dfbdd2553a139087bb3c
Naivrick <naivricka@gmail.com>
2023-09-02 21:00:03 +0500

    Update

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hi! Recursion =(")
+def a():
+    spam = 'Ant'
+    print(f"spam is {spam}")
+    b()
+    print(f"spam is {spam}")
+def b():
+    spam = 'Bobcat'
+    print(f"spam is {spam}")
+    c()
+    print(f"spam is {spam}")
+def c():
+    spam = 'Coyote'
+    print(f"spam is {spam}")
+
+a()
